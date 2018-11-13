679dd3a1bc5fb3a8b1e2febd419bcbd0a5edd890
Preston <hostetlerp@gmail.com>
2018-11-13 17:50:03 -0500

    working on controller

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,7 +2,13 @@ class SimplePagesController < ApplicationController
   def index
   end
 
+=begin
   def landing_page
     @featured_product = Product.first
+  end
+=end
+
+  def landing_page
+    @products = Product.limit(3)
   end
 end
