12ae3254cc1bf625fdad2d7c73866e9a89b418dd
Preston <hostetlerp@gmail.com>
2018-11-19 20:18:22 -0500

    fixing4.6

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,12 @@
 class OrdersController < ApplicationController
   def index
+    Order.create(user_id: 1, product_id: 1, total: 10.0)
+    Order.create(user_id: 2, product_id: 2, total: 5.0)
     @orders = Order.includes(:product).all
-    @order = Order.find(params[:id])
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def new
