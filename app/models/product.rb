c3b64875a3aaec0947d9490e41700299cef445ce
Preston <hostetlerp@gmail.com>
2018-11-25 15:40:25 -0500

    search bar

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   product = Product.create(name: "Test Product", description: "Lorem Ipsum", image_url: "http://careerfoundry.com/images/bike.jpg")
 end
+
+def self.search(search_term)
+  Product.where("name LIKE ?", "%#{search_term}%")
+end
