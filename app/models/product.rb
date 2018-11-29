129ef4b8ff1f45c94086720d981c58ddd5b6ce6d
Preston <hostetlerp@gmail.com>
2018-11-29 16:25:13 -0500

    comment

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,12 @@
 class Product < ApplicationRecord
   product = Product.create(name: "Test Product", description: "Lorem Ipsum", image_url: "http://careerfoundry.com/images/bike.jpg")
+  has_many :comments
 end
 
 def self.search(search_term)
   Product.where("name LIKE ?", "%#{search_term}%")
+end
+
+def highest_rating_comment
+  comments.rating_desc.first
 end
