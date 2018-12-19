214aa882ab11755006e3d79b5d30659140e721f0
Preston <hostetlerp@gmail.com>
2018-12-19 00:13:29 -0500

    fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,7 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     can :read :all # permissions for every user, even if not logged in
-    if user.present? # additional permissions for logged in users (they can manage)
-      can :manage, Post, user_id: user.id
-      if user.admin? # additional permissions for administrators
-        can :manage, :all
-      end
-    end  
+    user ||= User.new # guest user (not logged in)
   end
 end
 
