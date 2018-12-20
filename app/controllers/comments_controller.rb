840d07762284a92f9263712170b685fd3c2b5466
Preston <hostetlerp@gmail.com>
2018-12-19 23:07:30 -0500

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,16 +5,16 @@ class CommentsController < ApplicationController
 	    @comment.user = current_user
 	    @comment.save
 			@user = current_user
-	    redirect_to product_path(@product)
+	    redirect_to product_path(@product) and return
 
 
-			def respond_to do |format|
+			respond_to do |format|
 				if @comment.save
-					format.html { redirect_to @product, notice: 'Review was created successfully.' }
+					format.html { redirect_to (@product) and return, notice: 'Review was created successfully.' }
 					format.json { render :show, status: :created, location: @product }
 					format.js
 				else
-					format.html { redirect_to @product, alert: 'Review was not saved succeefully.' }
+					format.html { redirect_to (@product) and return, alert: 'Review was not saved succeefully.' }
 					format.json { render json: @comment.errors, status: :unprocessable_entity }
 				end
 			end
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
 			@comment = Comment.find(params[:id])
 			product = @comment.product
 			@comment.destroy
-			redirect_to product
+			redirect_to product and return
 	  end
 
 	  private
