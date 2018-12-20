2f3b667c0016df8418f7e8ce837962990f3997e4
Preston <hostetlerp@gmail.com>
2018-12-19 19:39:12 -0500

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,6 @@ class CommentsController < ApplicationController
 					format.html { redirect_to @product, notice: 'Review was created successfully.' }
 					format.json { render :show, status: :created, location: @product }
 					format.js
-				else
 					format.html { redirect_to @product, alert: 'Review was not saved succeefully.' }
 					format.json { render json: @comment.errors, status: :unprocessable_entity }
 				end
