b5be48b91a089fb17b297910ce640b4d912d7914
Preston <hostetlerp@gmail.com>
2018-12-24 02:03:12 -0500

    signin

--- app/controllers/users/user_registrations_controller.rb
+++ app/controllers/users/user_registrations_controller.rb
@@ -2,7 +2,7 @@ class UserRegistrationsController < Devise::RegistrationsController
   def create
     super
     if @user.persisted?
-      UserMailer?.welcome(@user).deliever_now
+      UserMailer.welcome(@user).deliever_now?
     end
   end
 end
