c9138aea5fe74d512fea9a14a57420302d6f671e
Preston <hostetlerp@gmail.com>
2019-01-09 21:24:03 -0500

    testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe User do
-  let(:user) { User.create!(email: "testing@gmail.com", password: "password") }
 
 
   it "should not validate users without an email addrees" do
     @user = FactoryBot.build(:user, email: "not_an_email")
-    expect(@user).to_not_be_valid
+    expect(@user).to_not be_valid
   end
+
 end
