1ef45080728e18aeb6e598b538bdcafe3ac4c4cb
Preston <hostetlerp@gmail.com>
2019-01-14 00:52:42 -0500

    payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,12 +12,9 @@ class PaymentsController < ApplicationController
         currency: "usd",
         source: token,
         description: params[:stripeEmail]
-        receipt_email: @user.email
       )
 
-      if charge.paid
-        Order.create!(product_id: @product.id, user_id: @user.id, total: @product.price)
-      end
+      
 
     rescue Stripe::CardError => e
       # The card has been declined
